a0924f36e4a308acf33e53a07ed53504a3c2b209
Ahtshayan26 <udayasanthiran.2019359@iit.ac.lk>
2023-03-25 08:29:30 +0530

    Initial Commit

---
+++ GitHubPush/GitHubPushApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubPushApp.swift
+//  GitHubPush
+//
+//  Created by Niranjan on 2023-03-25.
+//
+
+import SwiftUI
+
+@main
+struct GitHubPushApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
